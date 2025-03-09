8dee4a6a1e569d34fb9e028ab3b6402b91a5718e
luanheider <luanheider@hotmail.com>
2025-03-09 14:17:44 -0400

    adicionado client e service de email

---
+++ src/main/java/com/luanheider/bff_agendador_tarefas/infrastructure/client/EmailClient.java
@@ -0,0 +1,11 @@
+package com.luanheider.bff_agendador_tarefas.infrastructure.client;
+
+import com.luanheider.bff_agendador_tarefas.business.dto.out.TarefaDTOResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+@FeignClient(name = "notificacao", url = "${notificacao.url}")
+public interface EmailClient {
+
+    void enviarEmail(@RequestBody TarefaDTOResponse tarefaDTO);
+}
